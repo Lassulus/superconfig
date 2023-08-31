ba770d9013c4a0c1c189cfbeed4e8cc112e4c20d
lassulus <git@lassul.us>
2023-08-31 22:25:03 +0200

    tools.asvim: wrap via symlink in ~/.config

--- tools/astronvim/flake-module.nix
+++ tools/astronvim/flake-module.nix
@@ -73,23 +73,25 @@
       # does not build yet on aarch64
     ] ++ lib.optional (pkgs.stdenv.hostPlatform.system == "x86_64-linux") pkgs.deno; # lsp
     nvim_config = pkgs.runCommand "nvim_config" { } ''
-      mkdir -p $out/nvim/parser
+      mkdir -p $out/parser
 
-      ln -s ${inputs.astro-nvim}/* $out/nvim/
-      rm $out/nvim/lua
-      mkdir -p $out/nvim/lua
-      ln -s ${inputs.astro-nvim}/lua/* $out/nvim/lua
-      ln -s ${./user} $out/nvim/lua/user
+      ln -s ${inputs.astro-nvim}/* $out/
+      rm $out/lua
+      mkdir -p $out/lua
+      ln -s ${inputs.astro-nvim}/lua/* $out/lua
+      ln -s ${./user} $out/lua/user
 
       ${lib.concatMapStringsSep "\n" (name: ''
-        ln -s ${pkgs.tree-sitter.builtGrammars."tree-sitter-${name}"}/parser $out/nvim/parser/${name}.so
+        ln -s ${pkgs.tree-sitter.builtGrammars."tree-sitter-${name}"}/parser $out/parser/${name}.so
       '') langs}
     '';
   in {
     packages.asvim = pkgs.writers.writeDashBin "asvim" ''
+      set -efux
       unset VIMINIT
       export PATH=$PATH:${lib.makeBinPath nvim_deps}
-      XDG_CONFIG_HOME=${nvim_config} ${pkgs.neovim}/bin/nvim "$@"
+      ln -sfT ${nvim_config} "$HOME"/.config/lvim
+      NVIM_APPNAME=lvim ${pkgs.neovim}/bin/nvim "$@"
     '';
   };
 }
