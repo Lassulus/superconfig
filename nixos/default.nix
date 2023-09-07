40bf26a7a77959b73b75f449dd6a578c1edadf9d
lassulus <git@lassul.us>
2023-09-07 12:14:26 +0200

    nixos default: add comments

--- nixos/default.nix
+++ nixos/default.nix
@@ -5,9 +5,9 @@
     ./3modules
     self.inputs.stockholm.nixosModules.krebs
   ];
-  nixpkgs.config.packageOverrides = import ./5pkgs pkgs;
+  nixpkgs.config.packageOverrides = import ./5pkgs pkgs; # TODO move into packages
   nixpkgs.overlays = [
     self.inputs.stockholm.overlays.default
-    (import (self.inputs.stockholm.inputs.nix-writers + "/pkgs"))
+    (import (self.inputs.stockholm.inputs.nix-writers + "/pkgs")) # TODO get rid of that overlay
   ];
 }
