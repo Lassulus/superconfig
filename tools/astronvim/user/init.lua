8992650b4e159cdf2598296d83aeb29498861b30
lassulus <git@lassul.us>
2023-09-14 23:12:53 +0200

    tools vim: use github theme

--- tools/astronvim/user/init.lua
+++ tools/astronvim/user/init.lua
@@ -42,7 +42,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "quiet",
+  colorscheme = "github_dark_high_contrast",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
