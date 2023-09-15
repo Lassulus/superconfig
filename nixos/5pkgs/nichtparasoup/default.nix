a92cbedb00c2d197f219b5fd94a811f86f1427e1
lassulus <git@lassul.us>
2023-09-15 16:15:31 +0200

    pkgs nichtparasoup: fetch from github

--- nixos/5pkgs/nichtparasoup/default.nix
+++ nixos/5pkgs/nichtparasoup/default.nix
@@ -1,16 +1,15 @@
-{ stdenv, pkgs, ... }:
+{ pkgs, ... }:
 let
   py = pkgs.python3Packages.python.withPackages (p: [
     p.werkzeug
     p.beautifulsoup4
   ]);
-  src = ./nichtparasoup;
-  # src = pkgs.fetchFromGitHub {
-  #   owner = "k4cg";
-  #   repo = "nichtparasoup";
-  #   rev = "c6dcd0d";
-  #   sha256 = "10xy20bjdnd5bjv2hf6v5y5wi0mc9555awxkjqf57rk6ngc5w6ss";
-  # };
+  src = pkgs.fetchFromGitHub {
+    owner = "k4cg";
+    repo = "nichtparasoup";
+    rev = "c6dcd0d";
+    sha256 = "10xy20bjdnd5bjv2hf6v5y5wi0mc9555awxkjqf57rk6ngc5w6ss";
+  };
 in pkgs.writeDashBin "nichtparasoup" ''
   ${py}/bin/python ${src}/nichtparasoup.py "$@"
 ''
