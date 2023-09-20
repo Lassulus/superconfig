1a469f60071e8a50eab5fc0ceefa0d6b2ec20690
lassulus <git@lassul.us>
2023-09-20 13:37:18 +0200

    machines.prism: add {flex,flux}.lassul.us

--- nixos/machines/prism/config.nix
+++ nixos/machines/prism/config.nix
@@ -202,6 +202,36 @@ in {
           recommendedProxySettings = true;
         };
       };
+      services.nginx.virtualHosts."flex.lassul.us" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://radar.r:7878";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            auth_basic "Restricted Content";
+            auth_basic_user_file ${pkgs.writeText "flix-user-pass" ''
+              krebs:$apr1$1Fwt/4T0$YwcUn3OBmtmsGiEPlYWyq0
+            ''};
+          '';
+        };
+      };
+      services.nginx.virtualHosts."flux.lassul.us" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://sonar.r:8989";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            auth_basic "Restricted Content";
+            auth_basic_user_file ${pkgs.writeText "flix-user-pass" ''
+              krebs:$apr1$1Fwt/4T0$YwcUn3OBmtmsGiEPlYWyq0
+            ''};
+          '';
+        };
+      };
       services.nginx.virtualHosts."lassul.us" = {
         locations."^~ /flix/".extraConfig = ''
           if ($scheme != "https") {
