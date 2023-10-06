a66c0b1f1b1b72b7bceaa45952addb88d9ad3dd7
lassulus <lassulus@lassul.us>
2023-10-06 16:42:02 +0200

    update flake.lock

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "git+file:/home/lass/src/nixpkgs";
 
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
