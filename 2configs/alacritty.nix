2533cc73f13a072bab3cbb6892833f89b47964bd
lassulus <git@lassul.us>
2023-10-15 09:48:15 +0200

    alacritty: start all terminals in new session again

--- 2configs/alacritty.nix
+++ 2configs/alacritty.nix
@@ -47,7 +47,7 @@
     name = "alacritty";
     paths = [
       (pkgs.writeDashBin "alacritty" ''
-        ${pkgs.alacritty}/bin/alacritty --config-file /var/theme/config/alacritty.yaml msg create-window "$@" ||
+        # ${pkgs.alacritty}/bin/alacritty --config-file /var/theme/config/alacritty.yaml msg create-window "$@" ||
         ${pkgs.alacritty}/bin/alacritty --config-file /var/theme/config/alacritty.yaml "$@"
       '')
       pkgs.alacritty
