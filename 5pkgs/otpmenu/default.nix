f7ded874bd33a2fadd4bd4372f8067aac504c8a5
lassulus <git@lassul.us>
2023-10-15 09:50:06 +0200

    optmenu: use rofi, make wayland compatible

--- 5pkgs/otpmenu/default.nix
+++ 5pkgs/otpmenu/default.nix
@@ -3,9 +3,9 @@ pkgs.writers.writeDashBin "otpmenu" ''
 set -efux
 x=$(${pkgs.pass}/bin/pass git ls-files '*/otp.gpg' \
   | ${pkgs.gnused}/bin/sed 's:/otp\.gpg$::' \
-  | ${pkgs.dmenu}/bin/dmenu
+  | ${pkgs.rofi}/bin/rofi -dmenu
 )
 
 otp=$(${(pkgs.pass.withExtensions (ext: [ ext.pass-otp ]))}/bin/pass otp code "$x/otp")
-printf %s "$otp" | ${pkgs.wtype}/bin/wtype -s 1 - || printf %s "$otp" | ${pkgs.xdotool}/bin/xdotool type -f -
+printf %s "$otp" | ${pkgs.wtype}/bin/wtype -d 10 -s 200 - || printf %s "$otp" | ${pkgs.xdotool}/bin/xdotool type -f -
 ''
