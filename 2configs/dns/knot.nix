fad436d0e19d3d95b8089dc65cc59e70d90e3db2
lassulus <git@lassul.us>
2023-10-15 09:36:13 +0200

    2configs: init dns config

---
+++ 2configs/dns/knot.nix
@@ -0,0 +1,142 @@
+{ pkgs
+, config
+, lib
+, inputs
+, ...
+}:
+let
+  ip4 = "95.217.192.59";
+  ip6 = "2a01:4f9:4a:4f1a::2";
+  # acmeChallenge = domain:
+  #   pkgs.writeText "_acme-challenge.${domain}.zone" ''
+  #     @ 3600 IN SOA _acme-challenge.${domain}. ns1.lassul.us. 2023101213 7200 3600 86400 3600
+
+  #     $TTL 600
+
+  #     @ IN NS ns1.lassul.us.
+  #   '';
+  # dyndns = domain:
+  #   pkgs.writeText "${domain}.zone" ''
+  #     @ 3600 IN SOA ${domain}. ns1.lassul.us. 2023101213 7200 3600 86400 3600
+
+  #     $TTL 300
+
+  #     @ IN NS ns1.lassul.us.
+  #   '';
+in
+{
+  #content of the secret
+  #key:
+  #- id: acme
+  #  algorithm: hmac-sha256
+  #  secret: 00000000000000000000000000000000000000000000
+  #nix-shell -p knot-dns --run 'keymgr -t my_name hmac-sha256'
+  # clanCore.secrets.knot = {
+  #   secrets."knot-keys.conf" = {};
+  #   generator = ''
+  #     ${pkgs.knot-dns}/bin/keymgr -t  hmac-sha256
+  #   '';
+  # };
+
+  services.knot = {
+    enable = true;
+    # keyFiles = [
+    #   config.sops.secrets."knot-keys.conf".path
+    # ];
+    settings = {
+      server = {
+        listen = [ "${ip4}@53" "${ip6}@53" ];
+      };
+
+      remote = [
+        {
+          id = "hetzner_ip4_1";
+          address = "213.239.242.238@53";
+        }
+        {
+          id = "hetzner_ip4_2";
+          address = "213.133.105.6@53";
+        }
+        {
+          id = "hetzner_ip4_3";
+          address = "193.47.99.3@53";
+        }
+        {
+          id = "hetzner_ip6_1";
+          address = "2a01:4f8:0:a101::a:1@53";
+        }
+        {
+          id = "hetzner_ip6_2";
+          address = "2a01:4f8:d0a:2004::2@53";
+        }
+        {
+          id = "hetzner_ip6_3";
+          address = "2001:67c:192c::add:a3";
+        }
+      ];
+
+      # to generate TSIG key
+      # for i in host; do keymgr -t $i; done
+      acl = [
+        {
+          id = "hetzner_ip4_1";
+          address = "213.239.242.238";
+          action = "transfer";
+        }
+        {
+          id = "hetzner_ip4_2";
+          address = "213.133.105.6";
+          action = "transfer";
+        }
+        {
+          id = "hetzner_ip4_3";
+          address = "193.47.99.3";
+          action = "transfer";
+        }
+        {
+          id = "hetzner_ip6_1";
+          address = "2a01:4f8:0:a101::a:1";
+          action = "transfer";
+        }
+        {
+          id = "hetzner_ip6_2";
+          address = "2a01:4f8:d0a:2004::2";
+          action = "transfer";
+        }
+        {
+          id = "hetzner_ip6_3";
+          address = "2001:67c:192c::add:a3";
+          action = "transfer";
+        }
+      ];
+
+      mod-rrl = [{
+        id = "default";
+        rate-limit = 200;
+        slip = 2;
+      }];
+
+      policy = [{
+        id = "default";
+        algorithm = "RSASHA256";
+        ksk-size = 4096;
+        zsk-size = 2048;
+      }];
+
+
+      template = [
+      ];
+      zone = [
+        {
+          domain = "lassul.us";
+          file = "${./lassul.us.zone}";
+          notify = [ "hetzner_ip4_1" "hetzner_ip4_2" "hetzner_ip4_3" "hetzner_ip6_1" "hetzner_ip6_2" "hetzner_ip6_3" ];
+          acl = [ "hetzner_ip4_1" "hetzner_ip4_2" "hetzner_ip4_3" "hetzner_ip6_1" "hetzner_ip6_2" "hetzner_ip6_3" ];
+        }
+      ];
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 53 ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
+}
