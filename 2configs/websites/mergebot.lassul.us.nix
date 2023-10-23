d6b602a0be9723b4310e6cd430bc12725151e418
lassulus <git@lassul.us>
2023-10-23 12:03:42 +0200

    mergebot.lassul.us: increase timeouts

--- 2configs/websites/mergebot.lassul.us.nix
+++ 2configs/websites/mergebot.lassul.us.nix
@@ -6,6 +6,12 @@
       proxyPass = "http://ignavia.r:8081";
       proxyWebsockets = true;
       recommendedProxySettings = true;
+      extraConfig = ''
+        proxy_connect_timeout 600;
+        proxy_send_timeout 600;
+        proxy_read_timeout 600;
+        send_timeout 600;
+      '';
     };
   };
 }
