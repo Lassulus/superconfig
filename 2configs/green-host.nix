c18ac068a7b4d84ba198d2d51662a59c34d07636
lassulus <git@lassul.us>
2023-11-06 21:42:25 +0100

    green-host: fix startup command

--- 2configs/green-host.nix
+++ 2configs/green-host.nix
@@ -2,5 +2,10 @@
 {
   krebs.sync-containers3.containers.green = {
     sshKey = "${config.krebs.secret.directory}/green.sync.key";
+    startCommand = ''
+      export PATH=$PATH:${pkgs.git}/bin
+      until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#green switch
+    '';
   };
 }
