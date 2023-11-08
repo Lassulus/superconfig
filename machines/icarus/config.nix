248adbef20bc67e6632c7a878ccced41d6cccd69
lassulus <git@lassul.us>
2023-11-08 09:55:28 +0100

    machines.icarus: use xmonad from desktops

--- machines/icarus/config.nix
+++ machines/icarus/config.nix
@@ -6,7 +6,7 @@
     ../../2configs/retiolum.nix
     ../../2configs/git.nix
     ../../2configs/exim-retiolum.nix
-    ../../2configs/baseX.nix
+    ../../2configs/desktops/xmonad
     ../../2configs/pipewire.nix
     ../../2configs/browsers.nix
     ../../2configs/programs.nix
