698a72432a08b53d1f34865800b68cde852e00af
lassulus <git@lassul.us>
2023-11-11 16:44:26 +0100

    daedalus: get config from legacy hw x220

--- machines/daedalus/physical.nix
+++ machines/daedalus/physical.nix
@@ -7,6 +7,18 @@
   ];
 
   fileSystems = {
+    "/" = {
+      device = "/dev/pool/root";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/sda2";
+    };
+    "/home" = {
+      device = "/dev/mapper/pool-home";
+      fsType = "btrfs";
+      options = ["defaults" "noatime" "ssd" "compress=lzo"];
+    };
     "/bku" = {
       device = "/dev/mapper/pool-bku";
       fsType = "btrfs";
@@ -22,4 +34,16 @@
     SUBSYSTEM=="net", ATTR{address}=="08:11:96:0a:5d:6c", NAME="wl0"
     SUBSYSTEM=="net", ATTR{address}=="f0:de:f1:71:cb:35", NAME="et0"
   '';
+
+  boot = {
+    initrd.luks.devices.luksroot.device = "/dev/sda3";
+    initrd.availableKernelModules = [ "xhci_hcd" "ehci_pci" "ahci" "usb_storage" ];
+  };
+
+  services.logind.lidSwitch = "ignore";
+  services.logind.lidSwitchDocked = "ignore";
+
+  services.tlp.enable = true;
+
+  services.xserver.dpi = 80;
 }
