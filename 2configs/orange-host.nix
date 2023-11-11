2971047c963aeb5149646cd6349edfec4f1c6803
lassulus <git@lassul.us>
2023-11-11 16:43:07 +0100

    orange: build container from remote flake on startup

--- 2configs/orange-host.nix
+++ 2configs/orange-host.nix
@@ -2,6 +2,11 @@
 {
   krebs.sync-containers3.containers.orange = {
     sshKey = "${config.krebs.secret.directory}/orange.sync.key";
+    startCommand = ''
+      export PATH=$PATH:${pkgs.git}/bin
+      until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#orange switch
+    '';
   };
   containers.orange.bindMounts."/var/lib" = {
     hostPath = "/var/lib/sync-containers3/orange/state";
