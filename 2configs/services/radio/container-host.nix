eba3986c0966480d60ff93dd32ce4d6c45c8679e
lassulus <git@lassul.us>
2023-11-25 13:27:15 +0100

    radio container-host: dont write lock file

--- 2configs/services/radio/container-host.nix
+++ 2configs/services/radio/container-host.nix
@@ -8,7 +8,7 @@
     startCommand = ''
       export PATH=$PATH:${pkgs.git}/bin
       until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
-      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#radio switch
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#radio switch --no-write-lock-file
     '';
   };
   containers.radio = {
