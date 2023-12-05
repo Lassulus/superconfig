a243d557ea0b21ea5340aacbdb20bec992371c83
lassulus <git@lassul.us>
2023-12-05 20:26:05 +0100

    green-host: don't write lock file to update

--- 2configs/green-host.nix
+++ 2configs/green-host.nix
@@ -5,7 +5,7 @@
     startCommand = ''
       export PATH=$PATH:${pkgs.git}/bin
       until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
-      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#green switch
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#green switch --no-write-lock-file
     '';
   };
 }
