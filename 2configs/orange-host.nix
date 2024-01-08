3bf9894e300c68a243395e36d7889dd1307e53ae
lassulus <git@lassul.us>
2024-01-08 22:09:22 +0100

    orange: start with --no-write-lock-file

--- 2configs/orange-host.nix
+++ 2configs/orange-host.nix
@@ -5,7 +5,7 @@
     startCommand = ''
       export PATH=$PATH:${pkgs.git}/bin
       until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
-      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#orange switch
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#orange switch --no-write-lock-file
     '';
   };
   containers.orange.bindMounts."/var/lib" = {
