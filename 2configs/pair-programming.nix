91ae2e2a79610b3dc0a7b2cc6b330131435f2ddb
lassulus <git@lassul.us>
2024-01-08 22:09:39 +0100

    pair-programming: add more tools

--- 2configs/pair-programming.nix
+++ 2configs/pair-programming.nix
@@ -1,14 +1,33 @@
-{ self, config, ... }:
+{ self, config, pkgs, ... }:
 {
   users.users.pairprogramming = {
-    uid = self.inputs.stockholm.lib.genuid_uint31 "pairprogramming";
+    uid = self.inputs.stockholm.lib.genid_uint31 "pairprogramming";
     createHome = true;
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
       config.krebs.users.lass.pubkey
+      config.krebs.users.mic92.pubkey
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJ4yq7oHBO2iPs4xj797a//0ypnBr27sSadKUeL2NsK6AAAABHNzaDo=" # janik
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOYg513QZsVzoyVycXZjg4F3T3+OwtcY3WAhrlfyLgLTAAAABHNzaDo=" # janik
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBLZxVITpJ8xbiCa/u2gjSSIupeiqOnRh+8tFIoVhCON" # janik
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJXGRXiq61BQBUkQLBn720pzxiAZqchHWm504gWa2rE2" # kenji
     ];
   };
+
+  virtualisation.podman.enable = true;
+
+
+  environment.systemPackages = [
+    pkgs.ripgrep
+    pkgs.lazygit
+    pkgs.comma
+  ];
+
+  # clan dev related stuff
+  nix.settings.trusted-substituters = [
+    "https://cache.clan.lol"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
+  ];
 }
