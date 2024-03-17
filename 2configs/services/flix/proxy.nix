07049767955914a7e10448d0d0b6901004a49620
lassulus <git@lassul.us>
2024-03-17 06:19:16 +0100

    migrate fl[ieu]x.lassul.us to neoprism

--- 2configs/services/flix/proxy.nix
+++ 2configs/services/flix/proxy.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   services.nginx.virtualHosts."flix.lassul.us" = {
     forceSSL = true;
@@ -7,6 +7,36 @@
       proxyPass = "http://yellow.r:8096";
       proxyWebsockets = true;
       recommendedProxySettings = true;
+    };
+  };
+  services.nginx.virtualHosts."flex.lassul.us" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://radar.r:7878";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        auth_basic "Restricted Content";
+        auth_basic_user_file ${pkgs.writeText "flix-user-pass" ''
+          krebs:$apr1$1Fwt/4T0$YwcUn3OBmtmsGiEPlYWyq0
+        ''};
+      '';
+    };
+  };
+  services.nginx.virtualHosts."flux.lassul.us" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      proxyPass = "http://sonar.r:8989";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
+        auth_basic "Restricted Content";
+        auth_basic_user_file ${pkgs.writeText "flix-user-pass" ''
+          krebs:$apr1$1Fwt/4T0$YwcUn3OBmtmsGiEPlYWyq0
+        ''};
+      '';
     };
   };
 }
