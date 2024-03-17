0f11c413bf76092703272002282cf6e69eb7acae
lassulus <git@lassul.us>
2024-03-17 06:02:09 +0100

    gc: don't run on ignavia

--- 2configs/gc.nix
+++ 2configs/gc.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 {
   nix.gc = {
-    automatic = ! (lib.elem config.krebs.build.host.name [ "aergia" "mors" "xerxes" "coaxmetal" ] || config.boot.isContainer);
+    automatic = ! (lib.elem config.krebs.build.host.name [ "aergia" "ignavia" "mors" "xerxes" "coaxmetal" ] || config.boot.isContainer);
     options = "--delete-older-than 15d";
   };
 }
