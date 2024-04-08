1a7019e004352dc8ed52baf39dd959f2a28c8a6e
lassulus <git@lassul.us>
2024-04-08 19:52:21 +0200

    rework syncthing to not use stockholm

--- 2configs/syncthing.nix
+++ 2configs/syncthing.nix
@@ -2,17 +2,26 @@
 let
 
   shares = {
-    "/home/lass/sync" = [ "mors" "xerxes" "green" "blue" "coaxmetal" "aergia" "ignavia" ];
-    "/home/lass/tmp/the_playlist" = [ "mors" "phone" "prism" "omo" "radio" ];
+    "/home/lass/sync" = [ "mors" "green" "coaxmetal" "ignavia" ]; # TODO add aergia
+    "/home/lass/tmp/the_playlist" = [ "mors" "prism" "radio" ];
     # "/home/lass/.weechat" = [ "green" "mors" ];
-    "/home/lass/decsync" = [ "mors" "blue" "green" "phone" "massulus" ];
+    "/home/lass/decsync" = [ "mors" "green" "massulus" "massulus2" ];
   };
 
-  mk_peers = lib.mapAttrs (n: v: { id = v.syncthing.id; });
+  machine_peers = lib.foldr (host: acc:
+    if builtins.pathExists ../machines/${host}/facts/syncthing.pub then
+      acc // { ${host}.id = lib.removeSuffix "\n" (builtins.readFile ../machines/${host}/facts/syncthing.pub); }
+    else
+    acc
+  ) {} (lib.attrNames (builtins.readDir ../machines));
 
-  all_peers = lib.filterAttrs (n: v: v.syncthing.id != null) config.krebs.hosts;
+  all_peers = machine_peers // {
+    "massulus".id = "R2EGJ5S-PQMETUP-C2UGXQG-A6VP7TB-NGSN3MV-C7OGSWT-SZ34L3X-H6IF6AQ";
+    "massulus2".id = "OTJBJNO-VHBCVDL-QSELUS7-S6JDMYR-7WLE36A-6XSRSLP-VKYOYJJ-AHFGZAG";
+  };
+
   used_peer_names = lib.unique (lib.filter lib.isString (lib.flatten (lib.mapAttrsToList (n: v: v.devices) config.services.syncthing.settings.folders)));
-  used_peers = lib.filterAttrs (n: v: lib.elem n used_peer_names) all_peers;
+  used_peers = lib.filterAttrs (n: v: lib.elem n used_peer_names) (lib.traceValSeq all_peers);
 in {
   services.syncthing = {
     enable = true;
@@ -20,8 +29,7 @@ in {
     configDir = "/var/lib/syncthing";
     key = "${config.krebs.secret.directory}/syncthing.key";
     cert = "${config.krebs.secret.directory}/syncthing.cert";
-    # workaround for infinite recursion on unstable, remove in 23.11
-    settings.devices = mk_peers used_peers;
+    settings.devices = used_peers;
     settings.folders = lib.mapAttrs (share: devices: {
       enable = lib.elem config.networking.hostName devices;
       path = share;
