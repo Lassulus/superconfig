d2a5c345db967f0b988d53868fac623e7e33c42a
lassulus <git@lassul.us>
2024-05-14 15:33:55 +0200

    machines ubik: use clan facts for nextcloud pw

--- machines/ubik/config.nix
+++ machines/ubik/config.nix
@@ -34,12 +34,21 @@
     https = true;
     maxUploadSize = "9001M";
   };
+  clanCore.facts.services.nextcloud-pw = {
+    secret."nextcloud.pw" = { };
+    generator.script = ''
+      echo "$prompt_value" > "$secrets"/nextcloud.pw
+    '';
+    generator.prompt = ''
+      please enter the nextcloud admin password:
+    '';
+  };
   systemd.services.nextcloud-setup.serviceConfig.ExecStartPre = [
     "+${pkgs.writeDash "copy-pw" ''
       ${pkgs.rsync}/bin/rsync \
         --chown nextcloud:nextcloud \
         --chmod 0700 \
-        /var/src/secrets/nextcloud.pw /run/nextcloud.pw
+        ${config.clanCore.facts.services.nextcloud-pw.secret."nextcloud.pw".path} /run/nextcloud.pw
     ''}"
   ];
 
