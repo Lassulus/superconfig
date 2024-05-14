346f5dcd032d7d4421140cef585fff7736977a22
lassulus <git@lassul.us>
2024-05-14 15:35:52 +0200

    machines ubik: nextcloud 27 -> 28

--- machines/ubik/config.nix
+++ machines/ubik/config.nix
@@ -29,7 +29,7 @@
   services.nextcloud = {
     enable = true;
     hostName = "c.apanowicz.de";
-    package = pkgs.nextcloud27;
+    package = pkgs.nextcloud28;
     config.adminpassFile = "/run/nextcloud.pw";
     https = true;
     maxUploadSize = "9001M";
