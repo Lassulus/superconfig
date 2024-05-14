58dd0ee93dade71b51a13440caedbd63f10fc15d
lassulus <git@lassul.us>
2024-05-14 13:28:36 +0200

    ubik-host: fix with new facts interface

--- 2configs/ubik-host.nix
+++ 2configs/ubik-host.nix
@@ -2,6 +2,18 @@
 {
   krebs.sync-containers3.containers.ubik = {
     sshKey = "${config.krebs.secret.directory}/ubik.sync.key";
+    startCommand = ''
+      export PATH=$PATH:${pkgs.git}/bin
+      until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
+      ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#ubik switch --no-write-lock-file
+    '';
+  };
+  clanCore.facts.services.ubik-container = {
+    secret."ubik.sync.key" = { };
+    generator.script = ":";
+    generator.prompt = ''
+      copy or reference the secret key from the container into here, so we can actually start/sync the container
+    '';
   };
   containers.ubik.bindMounts."/var/lib" = {
     hostPath = "/var/lib/sync-containers3/ubik/state";
@@ -48,14 +60,5 @@
       proxyWebsockets = true;
       proxyPass = "https://prism.r";
     };
-  };
-  clanCore.facts.services.ubik-container = {
-    secret."ubik.sync.key" = { };
-    public."ubik.sync.pub" = { };
-    generator.path = with pkgs; [ coreutils openssh ];
-    generator.script = ''
-      ssh-keygen -t ed25519 -N "" -f "$secrets"/ubik.sync.key
-      mv "$secrets"/ubik.sync.key "$facts"/ubik.sync.pub
-    '';
   };
 }
