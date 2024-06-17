c048d5ec78199df679948ac52d19d6a229bcc9cc
lassulus <git@lassul.us>
2024-06-17 18:16:54 +0200

    tools vim: use cp from coreutils for osx compat

--- tools/astronvim/flake-module.nix
+++ tools/astronvim/flake-module.nix
@@ -74,7 +74,7 @@
       mkdir -p "$XDG_CONFIG_HOME/$NVIM_APPNAME" "$XDG_DATA_HOME"
       chmod -R u+w "$XDG_CONFIG_HOME/$NVIM_APPNAME"
       rm -rf "$XDG_CONFIG_HOME/$NVIM_APPNAME"
-      cp -arfT '${./config}'/ "$XDG_CONFIG_HOME/$NVIM_APPNAME"
+      ${pkgs.coreutils}/bin/cp -arfT '${./config}'/ "$XDG_CONFIG_HOME/$NVIM_APPNAME"
       chmod -R u+w "$XDG_CONFIG_HOME/$NVIM_APPNAME"
       ${pkgs.neovim}/bin/nvim --headless -c 'quitall' # install plugins
       mkdir -p "$XDG_DATA_HOME/$NVIM_APPNAME/lib/" "$XDG_DATA_HOME/$NVIM_APPNAME/site/"
