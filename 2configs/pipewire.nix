859c537d6cb0944d323a06a3043e68b10601b0b1
lassulus <git@lassul.us>
2024-07-01 17:31:59 +0200

    pipewire: remove obsolete workaround

--- 2configs/pipewire.nix
+++ 2configs/pipewire.nix
@@ -21,15 +21,4 @@
     pulse.enable = true;
     jack.enable = true;
   };
-
-  # workaround from https://github.com/NixOS/nixpkgs/pull/297806#issuecomment-2014059176
-  services.pipewire.wireplumber.configPackages = [
-    (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/59-systemwide-bluetooth.conf" ''
-      wireplumber.profiles = {
-        main = {
-          monitor.bluez.seat-monitoring = disabled
-        }
-      }
-    '')
-  ];
 }
