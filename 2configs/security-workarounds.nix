fa41b4d6b2c8f058fe0ea3d58e91ce578aa1a5c0
lassulus <git@lassul.us>
2024-07-01 12:37:37 +0200

    security-workarounds: add CVE-2024-6387

--- 2configs/security-workarounds.nix
+++ 2configs/security-workarounds.nix
@@ -5,5 +5,9 @@
       # https://github.com/NixOS/nixpkgs/pull/300028
       # NixOS is not affected
     }
+    { # CVE-2024-6387
+      # https://github.com/NixOS/nixpkgs/pull/323753#issuecomment-2199762128
+      services.openssh.settings.LoginGraceTime = 0;
+    }
   ];
 }
