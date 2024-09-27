01468833e880361a18dfe5cd519b23f1eb0af44f
lassulus <git@lassul.us>
2024-09-27 17:18:57 +0200

    nether: also activate data-mesher

--- 2configs/nether.nix
+++ 2configs/nether.nix
@@ -1,10 +1,14 @@
-{ self, lib, ... }:
+{ self, ... }:
 {
   imports = [
     self.inputs.nether.nixosModules.hosts
+    self.inputs.nether.nixosModules.mesher
   ];
   clan.networking.zerotier = {
     networkId = "ccc5da5295c853d4";
     name = "nether";
+  };
+  services.zerotierone.localConf = {
+    settings.interfacePrefixBlacklist = [ "retiolum" "wiregrill" "mycelium" ];
   };
 }
