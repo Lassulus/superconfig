4476591f38bd31a400659a393bd360bce41a2130
lassulus <git@lassul.us>
2024-09-27 14:42:43 +0200

    steam: run with more overrides

--- 2configs/steam.nix
+++ 2configs/steam.nix
@@ -1,30 +1,31 @@
-{ config, pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  #
-  # Steam stuff
-  # source: https://nixos.org/wiki/Talk:Steam
-  #
-  ##TODO: make steam module
-  # hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
-
-  # users.users.mainUser.packages = [ (pkgs.steam.override {
-  #   extraPkgs = p: with p; [
-  #     gnutls # needed for Halo MCC
-  #   ];
-  # }) ];
-  environment.systemPackages = [ pkgs.steam ];
-  hardware.opengl.driSupport32Bit = true;
-
-  #ports for inhome streaming
-  krebs.iptables = {
-    tables = {
-      filter.INPUT.rules = [
-        { predicate = "-p tcp --dport 27031"; target = "ACCEPT"; }
-        { predicate = "-p tcp --dport 27036"; target = "ACCEPT"; }
-        { predicate = "-p udp --dport 27031"; target = "ACCEPT"; }
-        { predicate = "-p udp --dport 27036"; target = "ACCEPT"; }
-      ];
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    package = pkgs.steam.override {
+      extraPkgs = (pkgs: with pkgs; [
+        gamemode
+        xorg.libXcursor xorg.libXi xorg.libXinerama xorg.libXScrnSaver
+        libpng libpulseaudio libvorbis stdenv.cc.cc.lib
+        libkrb5 keyutils
+        # additional packages...
+        # e.g. some games require python3
+      ]);
+        # extraLibraries = pkgs: [ pkgs.gperftools ];
+      # - Automatically enable gamemode whenever Steam is running
+      # -- NOTE: Assumes that a working system install of gamemode already exists!
+      extraProfile = let gmLib = "${lib.getLib(pkgs.gamemode)}/lib"; in ''
+        export LD_LIBRARY_PATH="${gmLib}:$LD_LIBRARY_PATH"
+      '';
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
+
+  programs.gamemode.enable = true;
 }
