e34dc0b9f5e879449def30505f5df3410069aab4
lassulus <git@lassul.us>
2024-09-27 14:41:08 +0200

    cast.lassul.us: open port & enable ssl

--- 2configs/cast.nix
+++ 2configs/cast.nix
@@ -4,9 +4,12 @@
   services.nginx = {
     enable = true;
     virtualHosts."cast.lassul.us" = {
+      enableACME = true;
+      forceSSL = true;
       locations."/".extraConfig = ''
         proxy_pass http://localhost:${toString config.services.owncast.port};
       '';
     };
   };
+  networking.firewall.allowedTCPPorts = [ config.services.owncast.rtmp-port ];
 }
