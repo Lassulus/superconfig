53bf925ec4fcd2af347ac88da88f0808ddb13a57
lassulus <git@lassul.us>
2024-10-16 08:42:01 +0200

    hass: gc podman containers after update

--- 2configs/hass/default.nix
+++ 2configs/hass/default.nix
@@ -45,6 +45,7 @@
       ${pkgs.podman}/bin/podman pull ${config.virtualisation.oci-containers.containers.homeassistant.image}
       ${pkgs.podman}/bin/podman stop homeassistant
       ${pkgs.podman}/bin/podman start homeassistant
+      ${pkgs.podman}/bin/podman system prune
     '';
   };
 
