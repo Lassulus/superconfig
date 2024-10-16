04180efd4cce0154f983bca3dd5d33a81c852a07
lassulus <git@lassul.us>
2024-10-16 08:54:01 +0200

    nvim: change cmp bindings

--- tools/nvim.nix
+++ tools/nvim.nix
@@ -265,9 +265,19 @@
               "<C-e>" = "cmp.mapping.abort()";
               "<C-b>" = "cmp.mapping.scroll_docs(-4)";
               "<C-f>" = "cmp.mapping.scroll_docs(4)";
-              "<C-Space>" = "cmp.mapping.complete()";
-              "<CR>" = "cmp.mapping.confirm({ select = true })";
-              "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+              "<CR>" = ''
+                cmp.mapping({
+                  i = function(fallback)
+                    if cmp.visible() and cmp.get_active_entry() then
+                      cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                    else
+                      fallback()
+                    end
+                  end,
+                  s = cmp.mapping.confirm({ select = true }),
+                  c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                })
+              '';
             };
           };
         };
