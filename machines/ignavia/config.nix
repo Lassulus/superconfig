5944e3e1c6e0aeba8cd054cf1c58caf30434f8d5
lassulus <git@lassul.us>
2024-11-05 19:09:00 +0100

    machines.ignavia: disable c-base config

--- machines/ignavia/config.nix
+++ machines/ignavia/config.nix
@@ -35,7 +35,7 @@
     # ../../2configs/sunshine.nix
     # ../../2configs/print.nix
     # ../../2configs/br.nix
-    ../../2configs/c-base.nix
+    # ../../2configs/c-base.nix
     ../../2configs/services/radio/tts.nix
   ];
 
