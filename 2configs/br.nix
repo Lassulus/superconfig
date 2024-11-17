86ea3c07d1525023fd585082fbfe61798158bb84
lassulus <git@lassul.us>
2024-11-17 01:32:04 +0100

    br: remove some legacy

--- 2configs/br.nix
+++ 2configs/br.nix
@@ -1,9 +1,5 @@
-{ pkgs, modulesPath, ... }:
+{ pkgs, ... }:
 {
-
-  imports = [
-    (modulesPath + "/services/hardware/sane_extra_backends/brscan4.nix")
-  ];
 
   hardware.sane = {
     enable = true;
@@ -30,18 +26,6 @@
       | ${pkgs.gawk}/bin/awk -v dev="*$name" '$1 == dev { print $2; exit }' \
       | ${pkgs.gnugrep}/bin/grep .
     '')
-  ];
-
-  services.printing = {
-    enable = true;
-    drivers = [
-      pkgs.mfcl2700dncupswrapper
-    ];
-  };
-
-  users.users.mainUser.extraGroups = [
-    "scanner"
-    "lp"
   ];
 
 }
