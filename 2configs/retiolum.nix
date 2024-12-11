4c6e4ea503da51ed51f2fe32309029a330b6cc25
lassulus <git@lassul.us>
2024-12-11 22:45:22 +0100

    retilum: migrate from facts to vars

--- 2configs/retiolum.nix
+++ 2configs/retiolum.nix
@@ -17,25 +17,26 @@
       AutoConnect = yes
       LocalDiscovery = yes
     '';
-    privkey = config.clanCore.facts.services.retiolum.secret."retiolum.rsa_key.priv".path;
-    privkey_ed25519 = config.clanCore.facts.services.retiolum.secret."retiolum.ed25519_key.priv".path;
+    privkey = config.clan.core.vars.generators.retiolum.files."retiolum.rsa_key.priv".path;
+    privkey_ed25519 = config.clan.core.vars.generators.retiolum.files."retiolum.ed25519_key.priv".path;
   };
 
-  clanCore.facts.services.retiolum = {
-    secret."retiolum.rsa_key.priv" = { };
-    secret."retiolum.ed25519_key.priv" = { };
-    public."retiolum.rsa_key.pub" = { };
-    public."retiolum.ed25519_key.pub" = { };
-    generator.path = with pkgs; [
+  clanCore.vars.generators.retiolum = {
+    files."retiolum.rsa_key.priv" = { };
+    files."retiolum.ed25519_key.priv" = { };
+    files."retiolum.rsa_key.pub".secret = false;
+    files."retiolum.ed25519_key.pub".secret = false;
+    migrateFact = "retiolum";
+    runtimeInputs = with pkgs; [
       coreutils
       tinc_pre
     ];
-    generator.script = ''
-      tinc --config "$secrets" generate-keys 4096 >/dev/null
-      mv "$secrets"/rsa_key.priv "$secrets"/retiolum.rsa_key.priv
-      mv "$secrets"/ed25519_key.priv "$secrets"/retiolum.ed25519_key.priv
-      mv "$secrets"/rsa_key.pub "$facts"/retiolum.rsa_key.pub
-      mv "$secrets"/ed25519_key.pub "$facts"/retiolum.ed25519_key.pub
+    script = ''
+      tinc --config "$out" generate-keys 4096 >/dev/null
+      mv "$out"/rsa_key.priv "$out"/retiolum.rsa_key.priv
+      mv "$out"/ed25519_key.priv "$out"/retiolum.ed25519_key.priv
+      mv "$out"/rsa_key.pub "$out"/retiolum.rsa_key.pub
+      mv "$out"/ed25519_key.pub "$out"/retiolum.ed25519_key.pub
     '';
   };
 
