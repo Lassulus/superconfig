4193865a89ca6ee780d1a690c1027bfaabfdbb46
lassulus <git@lassul.us>
2025-03-02 19:55:09 -0800

    machines.ignavia: enable fprintd

--- machines/ignavia/physical.nix
+++ machines/ignavia/physical.nix
@@ -1,4 +1,4 @@
-{ self, modulesPath, ... }:
+{ self, modulesPath, pkgs, ... }:
 {
   imports = [
     ./config.nix
@@ -7,8 +7,20 @@
     self.inputs.nixos-hardware.nixosModules.framework-13th-gen-intel
   ];
 
-  # fprintd is currently broken
-  services.fprintd.enable = false;
+  services.fprintd.enable = true;
+  services.fwupd.enable = true;
+  services.fwupd.extraRemotes = [ "lvfs-testing" ];
+  # Might be necessary once to make the update succeed
+  services.fwupd.uefiCapsuleSettings.DisableCapsuleUpdateOnDisk = true;
+  # we need fwupd 1.9.7 to downgrade the fingerprint sensor firmware
+  # we only need to downgrade the firmware once, so we can remove this once we have done that
+  # services.fwupd.package = (import (builtins.fetchTarball {
+  #   url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
+  #   sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
+  # }) {
+  #   inherit (pkgs) system;
+  # }).fwupd;
+
 
   krebs.power-action.battery = "BAT1";
 
