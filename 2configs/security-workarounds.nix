70f3414933ba62ec41ea181db14466da76e0f542
lassulus <git@lassul.us>
2025-05-14 20:20:47 +0200

    security-workarounds: remove old CVE

--- 2configs/security-workarounds.nix
+++ 2configs/security-workarounds.nix
@@ -6,10 +6,5 @@
       # https://github.com/NixOS/nixpkgs/pull/300028
       # NixOS is not affected
     }
-    {
-      # CVE-2024-6387
-      # https://github.com/NixOS/nixpkgs/pull/323753#issuecomment-2199762128
-      services.openssh.settings.LoginGraceTime = 0;
-    }
   ];
 }
