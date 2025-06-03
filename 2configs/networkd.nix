ab76cf42f023495148bebce9fa074e64c759b705
lassulus <git@lassul.us>
2025-06-03 05:40:18 +0200

    networkd: remove lgeacy wait-online overwrite

--- 2configs/networkd.nix
+++ 2configs/networkd.nix
@@ -1,9 +1,5 @@
 { lib, ... }:
 {
-  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
-  systemd.services.systemd-networkd.stopIfChanged = false;
-  # Services that are only restarted might be not able to resolve when resolved is stopped before
-  systemd.services.systemd-resolved.stopIfChanged = false;
 
   networking.useNetworkd = true;
   systemd.network = {
