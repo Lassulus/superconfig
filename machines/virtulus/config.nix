193cce4615bd4e2e7c8a963bf2da5c5911961fa0
lassulus <git@lassul.us>
2025-06-03 05:42:23 +0200

    machines.virtulus: add some config to install in standalone qemu

--- machines/virtulus/config.nix
+++ machines/virtulus/config.nix
@@ -1,8 +1,18 @@
+{ modulesPath, ... }:
 {
   imports = [
     ../../2configs
+    ../../2configs/spora.nix
+    (modulesPath + "/image/images.nix")
   ];
 
   krebs.build.host.name = "virtulus";
-  services.mingetty.autologinUser = "lass";
+  services.mingetty.autologinUser = "demo";
+
+  users.users.demo = {
+    isNormalUser = true;
+    password = "clanlol";
+  };
+
+  services.tor.enable = true;
 }
