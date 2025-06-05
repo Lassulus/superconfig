aec76ee5478debf4afcd0a7fca19a8c7e5b2136d
lassulus <lassulus@lassuluss-MacBook-Air.local>
2025-06-05 10:26:50 +0200

    tools.pass: add otp & nicer output

--- tools/pass.nix
+++ tools/pass.nix
@@ -1,25 +1,25 @@
-
 { self, ... }:
 {
   perSystem =
-    { pkgs, ... }: {
+    { pkgs, ... }:
+    {
       packages.pass = pkgs.symlinkJoin {
         name = "pass";
         paths = [
           (pkgs.writeShellApplication {
             name = "pass";
             runtimeInputs = [
-              pkgs.pass
+              (pkgs.pass.withExtensions (ext: [ ext.pass-otp ]))
               pkgs.gnupg
             ];
             text = ''
-              set -x
-              gpg --import ${self.keys.pgp.yubi.key}
-              echo '${self.keys.pgp.yubi.id}:6:' | gpg --import-ownertrust
+              set -efu
+              gpg --import ${self.keys.pgp.yubi.key} &>/dev/null
+              echo '${self.keys.pgp.yubi.id}:6:' | gpg --import-ownertrust &>/dev/null
               pass "$@"
             '';
           })
-          pkgs.pass
+          (pkgs.pass.withExtensions (ext: [ ext.pass-otp ]))
         ];
       };
     };
