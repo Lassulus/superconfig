6d6dec1867b18260b6e028a22b6da78e88d306d3
lassulus <git@lassul.us>
2025-07-06 13:54:03 +0200

    machines.barnacle: set EDITOR to nvim

--- machines/barnacle/configuration.nix
+++ machines/barnacle/configuration.nix
@@ -47,6 +47,10 @@
     pkgs.tea
   ];
 
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
