e7dc2192b6dcd556575051b07efc579b73f7f661
lassulus <git@lassul.us>
2025-08-09 14:46:55 +0200

    configs.ubik-host: migrate facts to vars

--- 2configs/ubik-host.nix
+++ 2configs/ubik-host.nix
@@ -1,18 +1,22 @@
 { config, pkgs, ... }:
 {
   krebs.sync-containers3.containers.ubik = {
-    sshKey = "${config.krebs.secret.directory}/ubik.sync.key";
+    sshKey = config.clan.core.vars.generators.ubik-container.files."ubik.sync.key".path;
     startCommand = ''
       export PATH=$PATH:${pkgs.git}/bin
       until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
       ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#ubik switch --no-write-lock-file
     '';
   };
-  clan.core.facts.services.ubik-container = {
-    secret."ubik.sync.key" = { };
-    generator.script = ":";
-    generator.prompt = ''
-      copy or reference the secret key from the container into here, so we can actually start/sync the container
+  clan.core.vars.generators.ubik-container = {
+    files."ubik.sync.key" = { };
+    migrateFact = "ubik-container";
+    prompts.key = {
+      description = "copy or reference the secret key from the container into here, so we can actually start/sync the container";
+      type = "hidden";
+    };
+    script = ''
+      cp "$prompts"/key "$out"/ubik.sync.key
     '';
   };
   containers.ubik.bindMounts."/var/lib" = {
