ed1efb6ddafc3c0e4a4671298e8815386e79a1e1
lassulus <git@lassul.us>
2025-08-09 14:46:20 +0200

    configs.shadowsocks: migrate facts to vars

--- 2configs/shadowsocks.nix
+++ 2configs/shadowsocks.nix
@@ -2,19 +2,21 @@
 {
   services.shadowsocks = {
     enable = true;
-    passwordFile = config.clan.core.facts.services.shadowsocks.secret.shadowsocks_password.path;
+    passwordFile = config.clan.core.vars.generators.shadowsocks.files.shadowsocks_password.path;
   };
   networking.firewall.allowedTCPPorts = [ 8388 ];
-  clan.core.facts.services.shadowsocks = {
-    secret."shadowsocks_password" = { };
-    generator = {
-      prompt = "please enter your shadowsocks password";
-      path = with pkgs; [
-        coreutils
-      ];
-      script = ''
-        echo "$prompt_value" > "$secrets"/shadowsocks_password
-      '';
+  clan.core.vars.generators.shadowsocks = {
+    files."shadowsocks_password" = { };
+    migrateFact = "shadowsocks";
+    prompts.password = {
+      description = "please enter your shadowsocks password";
+      type = "hidden";
     };
+    runtimeInputs = with pkgs; [
+      coreutils
+    ];
+    script = ''
+      cp "$prompts"/password "$out"/shadowsocks_password
+    '';
   };
 }
