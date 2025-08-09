035de370361374af3f1b7ae01e3245e72eea28fe
lassulus <git@lassul.us>
2025-08-09 14:41:15 +0200

    services.pad: migrate facts to vars

--- 2configs/services/pad/default.nix
+++ 2configs/services/pad/default.nix
@@ -2,19 +2,23 @@
 {
   imports = [ ./module.nix ];
   clan.hedgedoc.domain = "pad.lassul.us";
-  clan.core.facts.services.hedgedoc-github-auth = {
-    secret."hedgedoc.env" = { };
-    generator.script = ''
-      echo "$prompt_value" > "$secrets"/hedgedoc.env
-    '';
-    generator.prompt = ''
-      goto https://github.com/settings/ap:qplications/2352617 and paste the data in the following format:
-      GITHUB_CLIENT_ID=...
-      GITHUB_CLIENT_SECRET=...
+  clan.core.vars.generators.hedgedoc-github-auth = {
+    files."hedgedoc.env" = { };
+    migrateFact = "hedgedoc-github-auth";
+    prompts.env = {
+      description = ''
+        goto https://github.com/settings/applications/2352617 and paste the data in the following format:
+        GITHUB_CLIENT_ID=...
+        GITHUB_CLIENT_SECRET=...
+      '';
+      type = "multiline";
+    };
+    script = ''
+      cp "$prompts"/env "$out"/hedgedoc.env
     '';
   };
   # https://github.com/settings/applications/2352617
   systemd.services.hedgedoc.serviceConfig.EnvironmentFile = [
-    config.clan.core.facts.services.hedgedoc-github-auth.secret."hedgedoc.env".path
+    config.clan.core.vars.generators.hedgedoc-github-auth.files."hedgedoc.env".path
   ];
 }
