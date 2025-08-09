6f807b5a6ad7f5dc914dd6cecd32211d5ed80cb7
lassulus <git@lassul.us>
2025-08-09 14:41:33 +0200

    configs.green-host: migrate facts to vars

--- 2configs/green-host.nix
+++ 2configs/green-host.nix
@@ -1,17 +1,17 @@
 { config, pkgs, ... }:
 {
   krebs.sync-containers3.containers.green = {
-    sshKey = config.clanCore.facts.services.green-container.secret."green.sync.key".path;
+    sshKey = config.clan.core.vars.generators.green-container.files."green.sync.key".path;
     startCommand = ''
       export PATH=$PATH:${pkgs.git}/bin
       until ${pkgs.dig.host}/bin/host github.com; do sleep 1; done
       ${pkgs.nixos-rebuild}/bin/nixos-rebuild --refresh --flake github:lassulus/superconfig#green switch --no-write-lock-file
     '';
   };
-  clanCore.facts.services.green-container = {
-    secret."green.sync.key" = { };
-    generator.script = ":";
-    generator.prompt = ''
+  clan.core.vars.generators.green-container = {
+    files."green.sync.key" = { };
+    script = ":";
+    prompt = ''
       copy or reference the secret key from the container into here, so we can actually start/sync the container
     '';
   };
