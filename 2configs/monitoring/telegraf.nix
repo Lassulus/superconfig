76df05f93de26e4b7b0dd452b6f8daec1076a6c7
lassulus <git@lassul.us>
2025-08-09 14:40:30 +0200

    configs.monitoring.telegraf: nix fmt

--- 2configs/monitoring/telegraf.nix
+++ 2configs/monitoring/telegraf.nix
@@ -96,70 +96,69 @@ in
     enable = true;
     extraConfig = {
       agent.interval = "60s";
-      inputs =
-        {
-          prometheus.urls = lib.mkIf config.services.promtail.enable [
-            # default promtail port
-            "http://localhost:9080/metrics"
+      inputs = {
+        prometheus.urls = lib.mkIf config.services.promtail.enable [
+          # default promtail port
+          "http://localhost:9080/metrics"
+        ];
+        prometheus.metric_version = 2;
+        kernel_vmstat = { };
+        nginx.urls = lib.mkIf config.services.nginx.statusPage [
+          "http://localhost/nginx_status"
+        ];
+        smart = lib.mkIf (!isVM) {
+          path_smartctl = pkgs.writeShellScript "smartctl" ''
+            exec /run/wrappers/bin/sudo ${pkgs.smartmontools}/bin/smartctl "$@"
+          '';
+        };
+        system = { };
+        mem = { };
+        file = [
+          {
+            data_format = "influx";
+            file_tag = "name";
+            files = [ "/var/log/telegraf/*" ];
+          }
+        ]
+        ++ lib.optional (lib.any (fs: fs == "ext4") (lib.attrNames config.boot.supportedFilesystems)) {
+          name_override = "ext4_errors";
+          files = [ "/sys/fs/ext4/*/errors_count" ];
+          data_format = "value";
+        };
+        exec = [
+          {
+            ## Commands array
+            commands = [ ipv6DadCheck ] ++ zfsChecks ++ nfsChecks;
+            data_format = "influx";
+          }
+        ];
+        systemd_units = { };
+        swap = { };
+        disk.tagdrop = {
+          fstype = [
+            "tmpfs"
+            "ramfs"
+            "devtmpfs"
+            "devfs"
+            "iso9660"
+            "overlay"
+            "aufs"
+            "squashfs"
           ];
-          prometheus.metric_version = 2;
-          kernel_vmstat = { };
-          nginx.urls = lib.mkIf config.services.nginx.statusPage [
-            "http://localhost/nginx_status"
+          device = [
+            "rpc_pipefs"
+            "lxcfs"
+            "nsfs"
+            "borgfs"
           ];
-          smart = lib.mkIf (!isVM) {
-            path_smartctl = pkgs.writeShellScript "smartctl" ''
-              exec /run/wrappers/bin/sudo ${pkgs.smartmontools}/bin/smartctl "$@"
-            '';
-          };
-          system = { };
-          mem = { };
-          file =
-            [
-              {
-                data_format = "influx";
-                file_tag = "name";
-                files = [ "/var/log/telegraf/*" ];
-              }
-            ]
-            ++ lib.optional (lib.any (fs: fs == "ext4") (lib.attrNames config.boot.supportedFilesystems)) {
-              name_override = "ext4_errors";
-              files = [ "/sys/fs/ext4/*/errors_count" ];
-              data_format = "value";
-            };
-          exec = [
-            {
-              ## Commands array
-              commands = [ ipv6DadCheck ] ++ zfsChecks ++ nfsChecks;
-              data_format = "influx";
-            }
-          ];
-          systemd_units = { };
-          swap = { };
-          disk.tagdrop = {
-            fstype = [
-              "tmpfs"
-              "ramfs"
-              "devtmpfs"
-              "devfs"
-              "iso9660"
-              "overlay"
-              "aufs"
-              "squashfs"
-            ];
-            device = [
-              "rpc_pipefs"
-              "lxcfs"
-              "nsfs"
-              "borgfs"
-            ];
-          };
-          diskio = { };
-          zfs = {
-            poolMetrics = true;
-          };
-        }
-        // lib.optionalAttrs
+        };
+        diskio = { };
+        zfs = {
+          poolMetrics = true;
+        };
+      }
+      //
+        lib.optionalAttrs
           (
             if lib.versionAtLeast (lib.versions.majorMinor lib.version) "23.11" then
               config.boot.swraid.enable
