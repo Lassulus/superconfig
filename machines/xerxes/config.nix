b0a040df90e6eddd02f4b771bc977f173f76565c
lassulus <git@lassul.us>
2025-11-22 18:58:41 +0700

    machines.xerxes: fix deployment

--- machines/xerxes/config.nix
+++ machines/xerxes/config.nix
@@ -1,7 +1,15 @@
+{ self, pkgs, ... }:
 {
   imports = [
     ../../2configs
     ../../2configs/network-manager.nix
   ];
   system.stateVersion = "25.05";
+
+  krebs.build.host = self.inputs.stockholm.kartei.hosts.xerxes;
+
+  programs.sway.enable = true;
+  environment.systemPackages = [
+    pkgs.foot
+  ];
 }
