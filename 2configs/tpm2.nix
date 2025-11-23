e5c21fef2eb9e34932baf03a9c6bdb57a96ea3b4
lassulus <git@lassul.us>
2025-11-23 20:31:19 +0700

    tpm2: use ssh-tpm-agent

--- 2configs/tpm2.nix
+++ 2configs/tpm2.nix
@@ -1,10 +1,30 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+let
+  pinentry-askpass = pkgs.writeShellScriptBin "pinentry-askpass" ''
+    # Use pinentry with auto-detection (graphical if available, curses fallback)
+    result=$(cat <<EOF | ${pkgs.pinentry-all}/bin/pinentry
+SETDESC $1
+SETPROMPT Passphrase:
+GETPIN
+EOF
+    )
+    echo "$result" | grep '^D ' | cut -d' ' -f2-
+  '';
+in
+{
   security.tpm2 = {
     enable = true;
     pkcs11.enable = true;
     abrmd.enable = true;
   };
   environment.systemPackages = [
-    pkgs.tpm2-tools
+    pkgs.ssh-tpm-agent
+    pkgs.keyutils
+    pinentry-askpass
   ];
+  environment.variables = {
+    SSH_ASKPASS = lib.mkForce "/run/current-system/sw/bin/pinentry-askpass";
+    SSH_ASKPASS_REQUIRE = "force";
+  };
+  users.users.mainUser.extraGroups = [ "tss" ];
 }
