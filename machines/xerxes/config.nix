7ed0971f83a29f7ade5a7f9f8c77d9f6bf4a46f0
lassulus <git@lassul.us>
2025-11-23 20:31:01 +0700

    machines.xerxes: add firefox, pipewire, yubikey & tpm config

--- machines/xerxes/config.nix
+++ machines/xerxes/config.nix
@@ -3,13 +3,21 @@
   imports = [
     ../../2configs
     ../../2configs/network-manager.nix
+    ../../2configs/pipewire.nix
+    ../../2configs/yubikey.nix
+    ../../2configs/tpm2.nix
   ];
   system.stateVersion = "25.05";
 
   krebs.build.host = self.inputs.stockholm.kartei.hosts.xerxes;
 
   programs.sway.enable = true;
+  programs.firefox.enable = true;
   environment.systemPackages = [
-    pkgs.foot
+    pkgs.bitwarden-desktop
+    pkgs.rbw
+    pkgs.retroarch-free
+    pkgs.pavucontrol
+    pkgs.claude-code
   ];
 }
--- machines/xerxes/physical.nix
+++ machines/xerxes/physical.nix
@@ -8,4 +8,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.graphics.enable = true;
 }
