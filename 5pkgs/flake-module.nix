44b92f3276781d266398b0cbbdb9d8d738aa4aef
lassulus <git@lassul.us>
2025-11-27 08:42:33 +0100

    pkgs: filter by meta.platforms

--- 5pkgs/flake-module.nix
+++ 5pkgs/flake-module.nix
@@ -3,21 +3,26 @@
   perSystem =
     { lib, pkgs, ... }:
     {
-      packages = lib.mapAttrs (
-        name: _:
+      packages =
         let
-          pkgDir = ./. + "/${name}";
-          hasPackageNix = builtins.pathExists (pkgDir + "/package.nix");
-          hasDefaultNix = builtins.pathExists (pkgDir + "/default.nix");
+          allPackages = lib.mapAttrs (
+            name: _:
+            let
+              pkgDir = ./. + "/${name}";
+              hasPackageNix = builtins.pathExists (pkgDir + "/package.nix");
+              hasDefaultNix = builtins.pathExists (pkgDir + "/default.nix");
+            in
+            if hasPackageNix then
+              pkgs.callPackage (pkgDir + "/package.nix") { }
+            else if hasDefaultNix then
+              builtins.trace "Warning: ${name} is using deprecated default.nix, please rename to package.nix" (
+                pkgs.callPackage pkgDir { }
+              )
+            else
+              throw "Package ${name} has neither package.nix nor default.nix"
+          ) (lib.filterAttrs (_n: v: v == "directory") (builtins.readDir ./.));
         in
-        if hasPackageNix then
-          pkgs.callPackage (pkgDir + "/package.nix") { }
-        else if hasDefaultNix then
-          builtins.trace "Warning: ${name} is using deprecated default.nix, please rename to package.nix" (
-            pkgs.callPackage pkgDir { }
-          )
-        else
-          throw "Package ${name} has neither package.nix nor default.nix"
-      ) (lib.filterAttrs (_n: v: v == "directory") (builtins.readDir ./.));
+        # Filter packages based on meta.platforms
+        lib.filterAttrs (_name: pkg: lib.meta.availableOn pkgs.stdenv.hostPlatform pkg) allPackages;
     };
 }
