cd8329434c47fb36da3f49211d1b01602a357801
lassulus <git@lassul.us>
2025-12-02 10:31:39 +0100

    tools: init lockscreen

---
+++ tools/lockscreen/flake-module.nix
@@ -0,0 +1,21 @@
+{
+  perSystem =
+    { pkgs, lib, ... }:
+    {
+      packages.lockscreen = pkgs.writeShellApplication {
+        name = "lockscreen";
+        runtimeInputs = [
+          pkgs.ffmpeg
+          pkgs.coreutils
+          pkgs.swaylock-plugin
+          pkgs.mpvpaper
+        ];
+        text = ''
+          VIDEO=$(shuf -n1 -e "$HOME"/lockscreens/*.mp4)
+          DURATION=$(ffprobe -v error -show_entries format=duration -of csv=p=0 "$VIDEO" 2>/dev/null | cut -d. -f1)
+          START=$((RANDOM % ''${DURATION:-300}))
+          exec swaylock-plugin --command "mpvpaper -o \"no-audio loop start=$START\" '*' \"$VIDEO\"" "$@"
+        '';
+      };
+    };
+}
