cd7502d8f6fdf7dc03d613319869c6949b88f42b
lassulus <git@lassul.us>
2025-12-06 21:28:55 +0100

    machines.xerxes: add fancontrol

--- machines/xerxes/gpd-fan.nix
+++ machines/xerxes/gpd-fan.nix
@@ -32,4 +32,21 @@ in
 {
   boot.extraModulePackages = [ gpd-fan ];
   boot.kernelModules = [ "gpd_fan" ];
+
+  hardware.fancontrol = {
+    enable = true;
+    config = ''
+      INTERVAL=2
+      DEVPATH=hwmon2=devices/platform/gpd_fan hwmon6=devices/pci0000:00/0000:00:18.3
+      DEVNAME=hwmon2=gpdfan hwmon6=k10temp
+      FCTEMPS=hwmon2/pwm1=hwmon6/temp1_input
+      FCFANS=hwmon2/pwm1=hwmon2/fan1_input
+      MINTEMP=hwmon2/pwm1=45
+      MAXTEMP=hwmon2/pwm1=85
+      MINSTART=hwmon2/pwm1=40
+      MINSTOP=hwmon2/pwm1=30
+      MINPWM=hwmon2/pwm1=0
+      MAXPWM=hwmon2/pwm1=255
+    '';
+  };
 }
