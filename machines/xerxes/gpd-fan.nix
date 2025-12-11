fcc7185d94f6c92cf5dbd12cf41ec0d397871416
lassulus <git@lassul.us>
2025-12-11 18:59:59 +0100

    machines.xerxes: fix fancontrol

--- machines/xerxes/gpd-fan.nix
+++ machines/xerxes/gpd-fan.nix
@@ -31,15 +31,17 @@ let
 in
 {
   boot.extraModulePackages = [ gpd-fan ];
-  boot.kernelModules = [ "gpd_fan" ];
+  # Load modules in explicit order for stable hwmon numbering
+  # gpd_fan -> hwmon2, k10temp -> hwmon3 (loaded before ACPI devices)
+  boot.kernelModules = [ "gpd_fan" "k10temp" ];
 
   hardware.fancontrol = {
     enable = true;
     config = ''
       INTERVAL=2
-      DEVPATH=hwmon2=devices/platform/gpd_fan hwmon6=devices/pci0000:00/0000:00:18.3
-      DEVNAME=hwmon2=gpdfan hwmon6=k10temp
-      FCTEMPS=hwmon2/pwm1=hwmon6/temp1_input
+      DEVPATH=hwmon2=devices/platform/gpd_fan hwmon3=devices/pci0000:00/0000:00:18.3
+      DEVNAME=hwmon2=gpdfan hwmon3=k10temp
+      FCTEMPS=hwmon2/pwm1=hwmon3/temp1_input
       FCFANS=hwmon2/pwm1=hwmon2/fan1_input
       MINTEMP=hwmon2/pwm1=45
       MAXTEMP=hwmon2/pwm1=85
