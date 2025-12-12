46006e6118eac936d7998fe0a8ef458324d3990c
lassulus <git@lassul.us>
2025-12-12 11:13:48 +0100

    machines.xerxes: gpd_fan is in linux 6.18

--- machines/xerxes/gpd-fan.nix
+++ machines/xerxes/gpd-fan.nix
@@ -1,39 +1,10 @@
-{ config, pkgs, ... }:
-let
-  gpd-fan = config.boot.kernelPackages.callPackage (
-    { stdenv, kernel }:
-    stdenv.mkDerivation {
-      pname = "gpd-fan-driver";
-      version = "git";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "Cryolitia";
-        repo = "gpd-fan-driver";
-        rev = "a400c7d2d7b0bd150f0f7148a10eea816d16314e";
-        hash = "sha256-W+1OCiDxFHHAC9pPq1t6u4bLubj/4nNl2cep/HYYAgs=";
-      };
-
-      hardeningDisable = [ "pic" ];
-
-      nativeBuildInputs = kernel.moduleBuildDependencies;
-
-      makeFlags = [
-        "KERNEL_SRC=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
-      ];
-
-      installPhase = ''
-        runHook preInstall
-        install *.ko -Dm444 -t $out/lib/modules/${kernel.modDirVersion}/kernel/drivers/gpdfan
-        runHook postInstall
-      '';
-    }
-  ) { };
-in
 {
-  boot.extraModulePackages = [ gpd-fan ];
   # Load modules in explicit order for stable hwmon numbering
   # gpd_fan -> hwmon2, k10temp -> hwmon3 (loaded before ACPI devices)
-  boot.kernelModules = [ "gpd_fan" "k10temp" ];
+  boot.kernelModules = [
+    "gpd_fan"
+    "k10temp"
+  ];
 
   hardware.fancontrol = {
     enable = true;
