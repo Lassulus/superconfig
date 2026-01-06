9b266438fedc62a25f7b0976439895843d970758
lassulus <git@lassul.us>
2026-01-06 21:14:04 +0100

    machines.xerxes: fix suspend

--- machines/xerxes/physical.nix
+++ machines/xerxes/physical.nix
@@ -9,4 +9,13 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   hardware.graphics.enable = true;
+
+  # Disable Bluetooth USB autosuspend to prevent hardware errors/disconnects
+  # Disable VPE (Video Processing Engine) to fix black screen after suspend/resume
+  # on AMD Strix APUs. VPE fails to reset properly causing IB test timeouts.
+  # https://bugs.launchpad.net/ubuntu/+source/linux/+bug/2065365
+  boot.extraModprobeConfig = ''
+    options btusb enable_autosuspend=0
+    options amdgpu ip_block_mask=0xfffff7ff
+  '';
 }
