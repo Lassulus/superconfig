a7d2ab4274ffec0299c2b8925f25d7b40c8c1c3c
lassulus <git@lassul.us>
2026-01-11 12:21:52 +0100

    rtl-sdr: add support for RTL-SDR 4

--- 2configs/rtl-sdr.nix
+++ 2configs/rtl-sdr.nix
@@ -1,6 +1,22 @@
+{ self, pkgs, ... }:
 {
+  # Blacklist DVB-T driver so rtl-sdr can use the device
   boot.blacklistedKernelModules = [ "dvb_usb_rtl28xxu" ];
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="2838", GROUP="adm", MODE="0666", SYMLINK+="rtl_sdr"
-  '';
+
+  # Use rtl-sdr-blog for V4 support (also works with V3 and other RTL-SDRs)
+  # This installs udev rules for all known RTL-SDR devices
+  services.udev.packages = [ pkgs.rtl-sdr-blog ];
+
+  # Ensure plugdev group exists and mainUser is a member (used by rtl-sdr udev rules)
+  users.groups.plugdev = {};
+  users.users.mainUser.extraGroups = [ "plugdev" ];
+
+  # SDR and TETRA tools
+  environment.systemPackages = [
+    pkgs.rtl-sdr-blog  # V4 compatible drivers and tools
+    pkgs.gqrx          # SDR receiver with spectrum analyzer
+    pkgs.sox           # Audio processing for recordings
+    self.packages.${pkgs.system}.tetra-kit
+    self.packages.${pkgs.system}.tetra-receiver
+  ];
 }
