806cb56b4f8ebb1a28afb2aa4b02959b8b218a85
lassulus <git@lassul.us>
2026-01-11 12:21:24 +0100

    bitlbee: remove deprecated plugins

--- 2configs/bitlbee.nix
+++ 2configs/bitlbee.nix
@@ -6,8 +6,6 @@
     portNumber = 6666;
     plugins = [
       pkgs.bitlbee-facebook
-      pkgs.bitlbee-steam
-      pkgs.bitlbee-discord
     ];
     libpurple_plugins = [
       # pkgs.telegram-purple
