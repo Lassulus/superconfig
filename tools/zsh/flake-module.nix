0a707a4cf7a1297d4521ba4a12c33b1d1fcb0990
lassulus <git@lassul.us>
2026-01-24 14:24:57 +0100

    tools.zsh: use comma to run wget for nix-index update

--- tools/zsh/flake-module.nix
+++ tools/zsh/flake-module.nix
@@ -178,7 +178,7 @@
           filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr A-Z a-z)"
           mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
           # -N will only download a new version if there is an update.
-          wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
+          , wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
           ln -f $filename files
         }
 
