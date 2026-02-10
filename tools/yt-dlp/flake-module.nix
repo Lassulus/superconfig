7aebac885360928321893c7ad5ab79c5ff73266b
lassulus <git@lassul.us>
2026-02-10 22:21:37 +0100

    tools.yt-dlp: fix args handling

--- tools/yt-dlp/flake-module.nix
+++ tools/yt-dlp/flake-module.nix
@@ -3,11 +3,12 @@
     { pkgs, ... }:
     {
       packages.yt-dlp = pkgs.writeShellScriptBin "yt-dlp" ''
+        args=$@
         nix-shell -p '(yt-dlp.overrideAttrs (_: {
           src = builtins.fetchTree "github:yt-dlp/yt-dlp";
           patches = [];
           postPatch = "python devscripts/update-version.py 0.99";
-        }))' -p deno --run "yt-dlp \"$@\""
+        }))' -p deno --run "yt-dlp $args"
       '';
     };
 }
