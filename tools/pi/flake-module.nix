b522bce1ace32c177f25387c73948679fd5620a6
lassulus <git@lassul.us>
2026-02-17 11:47:04 +0100

    tools.pi: add wrapped pi with permissions and plan-mode plugins

---
+++ tools/pi/flake-module.nix
@@ -0,0 +1,63 @@
+{ self, ... }:
+{
+  perSystem =
+    { pkgs, system, ... }:
+    let
+      piPkg = self.legacyPackages.${system}.llm.pi;
+
+      # Pre-install pi plugins into a fake npm global prefix
+      pluginPrefix =
+        pkgs.runCommand "pi-plugins"
+          {
+            nativeBuildInputs = [
+              pkgs.nodejs
+              pkgs.cacert
+            ];
+            outputHashMode = "recursive";
+            outputHashAlgo = "sha256";
+            outputHash = "sha256-QZSVCJ0XirRz56v6ogxaB37c0bI8+OGEjrnqCFr/YI8=";
+            impureEnvVars = [
+              "http_proxy"
+              "https_proxy"
+            ];
+            SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+          }
+          ''
+            export HOME=$TMPDIR
+            export npm_config_prefix=$out
+            npm install -g pi-hooks shitty-extensions
+          '';
+    in
+    {
+      packages.pi = self.wrapLib.makeWrapper {
+        pkgs = pkgs;
+        package = piPkg;
+        runtimeInputs = [ pkgs.nodejs ];
+        wrapper =
+          { exePath, ... }:
+          ''
+            set -efu
+            export npm_config_prefix="${pluginPrefix}"
+
+            # Ensure settings.json has our plugins listed
+            SETTINGS_DIR="''${PI_CODING_AGENT_DIR:-$HOME/.pi/agent}"
+            SETTINGS_FILE="$SETTINGS_DIR/settings.json"
+            mkdir -p "$SETTINGS_DIR"
+
+            # Add packages to settings if not already present
+            if [ ! -f "$SETTINGS_FILE" ]; then
+              echo '{"packages":["npm:pi-hooks","npm:shitty-extensions"]}' > "$SETTINGS_FILE"
+            else
+              for pkg in "npm:pi-hooks" "npm:shitty-extensions"; do
+                if ! grep -q "$pkg" "$SETTINGS_FILE"; then
+                  ${pkgs.jq}/bin/jq --arg p "$pkg" '.packages = ((.packages // []) + [$p] | unique)' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp"
+                  mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
+                fi
+              done
+            fi
+
+            exec ${exePath} "$@"
+          '';
+      };
+    };
+}
