b3eaee290d7e9b2edd58e17b33b65a3e9b4a0fc6
lassulus <git@lassul.us>
2026-02-22 23:21:59 +0100

    games.jazz2: enable multiplayer support

--- games/jazz2/default.nix
+++ games/jazz2/default.nix
@@ -9,18 +9,27 @@
 }:
 
 let
+  jazz2-mp = jazz2.overrideAttrs (old: {
+    cmakeFlags = old.cmakeFlags ++ [
+      (lib.cmakeBool "WITH_MULTIPLAYER" true)
+    ];
+  });
+
   gameData = fetchurl {
     url = "https://archive.org/download/jazz-jackrabbit-2-1.24-the-secret-files-plus/Jazz%20Jackrabbit%202%201.24%20The%20Secret%20Files%20Plus.zip";
     hash = "sha256-ceeXiGUy5QDq1HQPo5gpyQe2odAK1F5frofBXegeXi8=";
     name = "jazz-jackrabbit-2-tsf.zip";
   };
 
-  sourceData = runCommandLocal "jazz2-source-data" {
-    nativeBuildInputs = [ unzip ];
-  } ''
-    mkdir -p "$out/Jazz² Resurrection/Source"
-    unzip -j -o ${gameData} -d "$out/Jazz² Resurrection/Source/"
-  '';
+  sourceData =
+    runCommandLocal "jazz2-source-data"
+      {
+        nativeBuildInputs = [ unzip ];
+      }
+      ''
+        mkdir -p "$out/Jazz² Resurrection/Source"
+        unzip -j -o ${gameData} -d "$out/Jazz² Resurrection/Source/"
+      '';
 
   wrapper = writeShellScript "jazz2" ''
     datadir="''${XDG_DATA_HOME:-$HOME/.local/share}/Jazz² Resurrection"
@@ -29,7 +38,7 @@ let
       rm -rf "$datadir/Source"
       ln -sfn "${sourceData}/Jazz² Resurrection/Source" "$datadir/Source"
     fi
-    exec ${lib.getExe jazz2} "$@"
+    exec ${lib.getExe jazz2-mp} "$@"
   '';
 in
 
@@ -45,6 +54,6 @@ stdenvNoCC.mkDerivation {
   '';
 
   meta = jazz2.meta // {
-    description = "Jazz Jackrabbit 2 (Jazz² Resurrection with game data)";
+    description = "Jazz Jackrabbit 2 (Jazz² Resurrection with game data and multiplayer)";
   };
 }
