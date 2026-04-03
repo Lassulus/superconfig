05182e8f01dadce173016acebfee5807e0092275
lassulus <git@lassul.us>
2026-04-03 14:17:46 +0200

    2configs/services/flix: remove bruellwuerfel

--- 2configs/services/flix/default.nix
+++ 2configs/services/flix/default.nix
@@ -163,7 +163,6 @@
         root = "/var/download";
         extraConfig = ''
           fancyindex on;
-          fancyindex_footer "/fancy.html";
           include ${pkgs.nginx}/conf/mime.types;
           include ${pkgs.writeText "extrMime" ''
             types {
@@ -173,84 +172,7 @@
           create_full_put_path on;
         '';
       };
-      locations."/chatty" = {
-        proxyPass = "http://localhost:3000";
-        extraConfig = ''
-          rewrite /chatty/(.*) /$1  break;
-          proxy_set_header Host $host;
-        '';
-      };
-      locations."= /fancy.html".extraConfig = ''
-        alias ${pkgs.writeText "nginx_footer" ''
-          <div id="mydiv">
-            <!-- Include a header DIV with the same name as the draggable DIV, followed by "header" -->
-            <div id="mydivheader">Click here to move</div>
-              <iframe src="/chatty/index.html"></iframe>
-          </div>
-          <style>
-          #mydiv {
-            position: absolute;
-            z-index: 9;
-            background-color: #f1f1f1;
-            border: 1px solid #d3d3d3;
-            text-align: center;
-          }
 
-          #mydivheader {
-            padding: 10px;
-            cursor: move;
-            z-index: 10;
-            background-color: #2196F3;
-            color: #fff;
-          }
-          </style>
-          <script>
-            // Make the DIV element draggable:
-            dragElement(document.getElementById("mydiv"));
-
-            function dragElement(elmnt) {
-              var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-              if (document.getElementById(elmnt.id + "header")) {
-                // if present, the header is where you move the DIV from:
-                document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-              } else {
-                // otherwise, move the DIV from anywhere inside the DIV:
-                elmnt.onmousedown = dragMouseDown;
-              }
-
-              function dragMouseDown(e) {
-                e = e || window.event;
-                e.preventDefault();
-                // get the mouse cursor position at startup:
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                document.onmouseup = closeDragElement;
-                // call a function whenever the cursor moves:
-                document.onmousemove = elementDrag;
-              }
-
-              function elementDrag(e) {
-                e = e || window.event;
-                e.preventDefault();
-                // calculate the new cursor position:
-                pos1 = pos3 - e.clientX;
-                pos2 = pos4 - e.clientY;
-                pos3 = e.clientX;
-                pos4 = e.clientY;
-                // set the element's new position:
-                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-              }
-
-              function closeDragElement() {
-                // stop moving when mouse button is released:
-                document.onmouseup = null;
-                document.onmousemove = null;
-              }
-            }
-          </script>
-        ''};
-      '';
     };
     virtualHosts."jelly.r" = {
       enableACME = true;
@@ -319,29 +241,6 @@
       printable = "no";
     };
   };
-
-  systemd.services.bruellwuerfel =
-    let
-      bruellwuerfelSrc = pkgs.fetchFromGitHub {
-        owner = "krebs";
-        repo = "bruellwuerfel";
-        rev = "dc73adf69249fb63a4b024f1f3fbc9e541b27015";
-        sha256 = "078jp1gbavdp8lnwa09xa5m6bbbd05fi4x5ldkkgin5z04hwlhmd";
-      };
-    in
-    {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-      environment = {
-        IRC_CHANNEL = "#flix";
-        IRC_NICK = "bruelli";
-        IRC_SERVER = "irc.r";
-        IRC_HISTORY_FILE = "/tmp/bruelli.history";
-      };
-      serviceConfig = {
-        ExecStart = "${pkgs.deno}/bin/deno run -A ${bruellwuerfelSrc}/src/index.ts";
-      };
-    };
 
   networking.firewall.allowedTCPPorts = [
     80 # nginx
